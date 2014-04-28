c4f26186a1b4e3c3bec118d99f74dbcaff781154
BohemianHacks <jeff.blecha@gmail.com>
2014-04-28 08:51:26 -0500

    Create ticker.py

---
+++ ticker.py
@@ -0,0 +1,51 @@
+import json
+import urllib2
+import curses
+import curses.ascii
+import time
+
+DogeChainUrl = 'https://dogechain.info/chain/Dogecoin/q/addressbalance/'
+ExchangeUrl = 'https://www.dogeapi.com/wow/?a=get_current_price&convert_to=USD&amount_doge=1'
+address = 'DN395oxmaPTKFoB5fWUh8cD7qrx3HAM3UP'
+interval = 30.0
+
+def getExchangeRate():
+    try:
+        data = float(json.loads(urllib2.urlopen(ExchangeUrl).read())['data']['amount'])
+    except:
+        data = 0.0
+    return data
+
+def getBalance(address):
+    try:
+        data = float(urllib2.urlopen(DogeChainUrl+address).read())
+    except:
+        data = 0.0
+    return data
+
+win = curses.initscr()
+win.addstr("Loading...")
+win.refresh()
+win.nodelay(1)
+curses.cbreak()
+curses.noecho()
+curses.curs_set(0)
+key = win.getch()
+last = time.time()
+
+while key != curses.ascii.ESC:
+    balance = "$" + str(getBalance(address) * getExchangeRate())
+    win.clear()
+    win.addstr(balance)
+    win.refresh()
+
+    while time.time() - last < interval and key != curses.ascii.ESC:
+        curses.napms(200)
+        key = win.getch()
+
+    last = time.time()
+
+curses.nocbreak()
+curses.echo()
+curses.curs_set(1)
+curses.endwin()
